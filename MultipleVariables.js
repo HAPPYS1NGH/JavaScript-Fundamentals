651d89dec8c1773ccbbedf73a272f08d2240fa98
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2022-12-13 14:43:22 +0530

    Update MultipleVariables.js

--- MultipleVariables.js
+++ MultipleVariables.js
@@ -1,5 +1,12 @@
+/* 
+ * Create two variables a and b with the same value in each.
+ * First declare a and then set b equal to a.
+ *
+/
+
+
+
 const a=15;
- 
 const b=a;
 
 module.exports = { a, b }
