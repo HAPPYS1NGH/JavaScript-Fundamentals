3b6f801a4245335e6c3c0797c5668258ebf04103
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2022-12-15 20:06:53 +0530

    Added Functions

---
+++ FunctionsAndOperators/Average.js
@@ -0,0 +1,11 @@
+/*
+Find the average of four numerical values a, b, c, and d.
+
+Once you have found the average, be sure to return it.
+*/
+
+function average(a, b, c, d) {
+    return (a+b+c+d)/4;
+}
+
+module.exports = average;
---
+++ FunctionsAndOperators/Random.js
@@ -0,0 +1,11 @@
+/*
+Inside getRandom, get a random number from the Math.random() function.
+
+Then, return that number!
+*/
+
+function getRandom() {
+    return Math.random()
+}
+
+module.exports = getRandom;
