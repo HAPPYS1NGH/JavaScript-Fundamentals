afff6444f647cb9aac0796685c3232d4d75a4403
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2022-12-20 01:11:02 +0530

    Added numberOfKeys in Object

---
+++ Data Storage/Arrays/Objects/numberofKeys.js
@@ -0,0 +1,9 @@
+/*
+Given an object, find the number of keys inside the object. Return this number.
+*/
+
+function numberOfKeys(object) {
+    return Object.keys(object).length;
+}
+
+module.exports = numberOfKeys;
