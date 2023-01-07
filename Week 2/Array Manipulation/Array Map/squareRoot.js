3f1d75b878c9eb8e48d37debad0bc2e91f4e7f0b
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2023-01-07 18:36:00 +0530

    Added Square root of array

---
+++ Week 2/Array Manipulation/Array Map/squareRoot.js
@@ -0,0 +1,13 @@
+/**
+ * Using the squareRoot function, map each element in the array arr to its square root. 
+ * Return the new array.
+ */
+
+function squareRoot(arr) {
+    let a = arr.map((ele) =>
+        ele = Math.sqrt(ele)
+    )
+    return a
+}
+
+module.exports = squareRoot;
