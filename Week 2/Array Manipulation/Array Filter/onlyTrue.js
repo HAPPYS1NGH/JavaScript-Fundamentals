f6bd91cc55393882dfc0ccae7bfada566d71ae41
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2023-01-20 23:56:01 +0530

    Create onlyTrue.js

---
+++ Week 2/Array Manipulation/Array Filter/onlyTrue.js
@@ -0,0 +1,10 @@
+/*
+Given an array of booleans, keep only the true values:
+*/
+function onlyTrue(array) {
+   return array.filter(el =>{
+       return el
+   })  
+}
+
+module.exports = onlyTrue;
