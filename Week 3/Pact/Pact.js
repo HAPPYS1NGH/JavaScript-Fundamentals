85f72d5fd4549b7ab065565cb11a88719aaeaeae
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2023-01-26 19:28:35 +0530

    Added Pact

---
+++ Week 3/Pact/Pact.js
@@ -0,0 +1,13 @@
+// Your first task is to create two class methods catch and then. 
+// These methods are used by promises to wire up callbacks.
+class Pact {
+    // add methods to return on the instance
+   catch(){
+
+    }
+    then(){
+
+    }
+}
+
+module.exports = Pact;
